f9fa02e8b337d29dc1d879c2bf2ac980a3af14be
gilbertjacke <gilbertjacke@gmail.com>
2016-12-15 15:18:31 +0000

    search results returning

--- getPodcasts.js
+++ getPodcasts.js
@@ -1,11 +1,16 @@
 const itunes = require('itunes-search');
 
-const option = {
+const options = {
+    term: "Giant Bombcast",
   media: "podcast",
   entity: "podcast",
 }
 
-itunes.search( "Giant Bombcast", options,
-function(response) {
-  // do stuff with 'response'
-})
+itunes.search(options, "Giant Bombcast",
+function(err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(response);
+  }
+});
